0a90d7366ee362fbe20e5eeef0a84ac01affe327
Jeff Holst <33558430+jeffholst@users.noreply.github.com>
2023-03-30 20:16:03 -0500

    chore: GitHub Pages

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "SVG Belt",
+  base: "/svg-belt/",
   description: "Demo and documentation for SVG Belt",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
